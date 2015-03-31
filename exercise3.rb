c6c34a32ba12a39e062ae1c3ffd0e42be6273ef2
Tom Billingsley <tom.billingsley@gmail.com>
2015-03-31 13:59:25 -0400

    prompt user for YOB for age computation

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,10 @@
 puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}!"
+
+# For the last step, try asking your user how old they are
+puts "How old are you, #{name}?"
+age = gets.chomp
+#  and have your program output what year they were born in.
+puts 2015 - age.to_i
+
