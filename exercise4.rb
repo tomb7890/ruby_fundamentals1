30d665592b98186d95a0f63f19c9cc25350cf8f3
Tom Billingsley <tom.billingsley@gmail.com>
2015-03-31 14:15:56 -0400

    fixed behaviour for problem 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,7 +1,16 @@
+# Write a program in a file called exercise4.rb that outputs the
+# numbers from 1 to 100. For multiples of three, output "Bit" instead
+# of the number and for multiples of five, output "Maker". For numbers
+# which are multiples of both three and five, output "BitMaker".
+
 (1..100).each do |n|
-  if 0 == n % 3 
-    puts "Bit"
+  print "#{n}: " 
+  if (0 == n % 3) && (0 == n % 5 ) 
+    print "Bitmaker"
+  elsif 0 == n % 3 
+    print "Bit"
   elsif 0 == n % 5 
-    puts "Maker"
+    print "Maker"
   end
+  puts ""
 end
